05dd50f8ad3225af3d175582ca9727036202b2b0
Brittany <brittany.o.williams@gmail.com>
2023-08-03 20:44:55 -0600

    Image modal first go working

---
+++ components/image_popup.js
@@ -0,0 +1,8 @@
+$("#insights").on("click", function(){
+    $(".modal").attr("style","display:block");
+    
+});
+
+$(".modal").on("click", function(){
+    $(".modal").attr("style","display:none");
+});
